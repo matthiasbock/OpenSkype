c94d511921bfcc520ceb15fd6437b007a95e0b8a
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-08-11 12:36:33 +0200

    write file

--- rc4bruteforce.py
+++ rc4bruteforce.py
@@ -13,7 +13,7 @@ class Rambo:
 	def crack(self, start=0x00000000L):
 		finalseed = start
 
-		last_print = 0
+		last_write = 0
 		crc_correct = False
 		while not crc_correct:
 
@@ -24,11 +24,12 @@ class Rambo:
 			crc_correct = crc == self.crc
 			if not crc_correct:
 				finalseed += 1
-				if finalseed-last_print > 1000:
-					print long2hex(finalseed)
-					last_print = finalseed
+				if finalseed-last_write > 100000:
+					open('currentfinalseed','w').write(long2hex(finalseed))
+					last_write = finalseed
 
 		print long2hex(finalseed)+' - cracked'
+		open('finalseed','w').write(long2hex(finalseed))
 
 
 
