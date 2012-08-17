ed5034a3f015d98f64bb094683960708c2197cc8
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-08-17 11:15:36 +0200

    decompression works

--- huffman.py
+++ huffman.py
@@ -4,57 +4,98 @@
 # Skype uses arithmetic compression:
 # the Huffman algorithm
 #
-# implementation of the algorithm described in "Silver needle in the Skype"
+# Hint: the picture, presented in "Silver needle in the Skype",
+# does not represent the frequency dictionary of "ACAB"
 #
 
-def makedictionary(s, debug=False):
-	frequency = {}
+def getfrequencies(s, debug=False):
+	frequencies = {}
 	for c in s:
-		if c in frequency.keys():
-			frequency[c] += 1
+		if c in frequencies.keys():
+			frequencies[c] += 1
 		else:
-			frequency[c] = 1
+			frequencies[c] = 1
 	if debug:
-		print '\tsymbol frequencies: '+str(frequency)
+		print '\tsymbol frequencies: '+str(frequencies)
+	return frequencies
 
-	keys = sorted(frequency.keys())
-	interval = 1.0/len(s)
+def frequencies2dictionary(length, frequencies, debug=False):
+	keys = sorted(frequencies.keys())
+	interval = 1.0/length
 	dictionary = {}
 	start = 0.0
 	for k in keys:
-		step = frequency[k]*interval
+		step = frequencies[k]*interval
 		dictionary[k] = [start, start+step]
 		start += step
 	if debug:
-		print '\tsymbol interval dictionary: '+str(dictionary)
-
+		print '\tsymbol dictionary: '+str(dictionary)
 	return dictionary
 
-def subdivide(r, interval):
-	start	= r[0]
-	stop	= r[1]
-	width	= stop-start
-	istart	= interval[0]
-	istop	= interval[1]
-	iwidth	= istop-istart
-	return [ start+(width*istart), start+(width*istop)  ]
+def makedictionary(s, debug=False):
+	return frequencies2dictionary( len(s), getfrequencies(s, debug), debug )
 
+#
+# Example usage:
+#  interval = [0.5, 0.75]
+#  subinterval = [0.4, 0.6]
+#  return [0.5+0.4*0.25, 0.5+0.6*0.25]
+#
+def subdivide(interval, subinterval):
+	start	= interval[0]
+	stop	= interval[1]
+	width	= stop-start
+	istart	= subinterval[0]
+	istop	= subinterval[1]
+#	iwidth	= istop-istart
+	return [ start+(istart*width), start+(istop*width)  ]
+
+#
+# compress string to real
+#
 def compress(s, debug=False):
+	l = len(s)
 	if debug:
 		print 'compressing "'+s+'" ...'
-		print '\tstring length: '+str(len(s))
+		print '\tstring length: '+str(l)
 	d = makedictionary(s, debug)
 	r = [0, 1]
 	for c in s:
 		r = subdivide(r, d[c])
 		if debug:
 			print '\t'+c+': '+str(r)
-	return r
+	return r[0],d,l
+
+#
+# decompress real to string
+#
+def decompress(r, dictionary, length, debug=False):
+	if debug:
+		print 'decompressing "'+str(r)+'" ...'
+		print '\tstring length: '+str(l)
+		print '\tsymbol dictionary: '+str(dictionary)
+	s = ''
+	interval = [0, 1]
+	from copy import deepcopy
+	applied_dictionary = deepcopy(dictionary)
+	for i in range(length):
+		for k in applied_dictionary.keys():	# which is the next char ?
+			subinterval = applied_dictionary[k]
+			if r >= subinterval[0] and (r < subinterval[1] or (r == 1 and subinterval[1] == 1)):
+				s += k
+				break
+		if debug:
+			print '\t'+str(r)+' is in '+str(subinterval)+' -> '+k
+		for k in dictionary.keys():
+			applied_dictionary[k] = subdivide(subinterval, dictionary[k])
+	return s
 
 if __name__ == '__main__':
 	import sys
 	try:
-		compress(sys.argv[1], debug=True)
+		r,d,l = compress(sys.argv[1], debug=True)
 	except:
-		compress('ACABCACA', debug=True)
+		r,d,l = compress('ACABCACA', debug=True)
+
+	decompress(r, d, l, debug=True)
 
